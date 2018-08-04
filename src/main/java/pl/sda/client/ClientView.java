6bb5539a1d758b2e3117624fc993c82bd3288490
Marek Ciesla <ciesla.marek@gmail.com>
2018-08-04 15:55:07 +0200

    first commit

---
+++ src/main/java/pl/sda/client/ClientView.java
@@ -0,0 +1,7 @@
+package pl.sda.client;
+
+/**
+ * Created by MCK on 04.08.2018 14:52
+ **/
+public class ClientView {
+}
---
+++ src/main/java/pl/sda/server/ChatServer.java
@@ -0,0 +1,7 @@
+package pl.sda.server;
+
+/**
+ * Created by MCK on 04.08.2018 14:53
+ **/
+public class ChatServer {
+}
